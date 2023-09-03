02fd4ce06c9495aa5f285f2122c18ac9b1668594
Staravoy <85824511+Staravoy@users.noreply.github.com>
2023-09-03 21:33:24 +0300

    Вже отриманні перші дані

--- test_parce.py
+++ test_parce.py
@@ -1,1 +1,32 @@
-print("Hello World!!!")
+import requests
+from bs4 import BeautifulSoup
+import re
+
+# URL сторінки
+url = "https://minfin.com.ua/ua/currency/"
+
+# Відправити GET-запит
+response = requests.get(url)
+
+# Перевірка, чи вдалося отримати сторінку
+if response.status_code == 200:
+    # Розбираємо HTML-код сторінки за допомогою BeautifulSoup
+    soup = BeautifulSoup(response.text, 'html.parser')
+    # Знаходимо елемент з певним класом (замість "class_name" вставте потрібний клас)
+    class_name = "dKDsVV"  # Замініть на потрібний клас
+    element_with_class = soup.find_all(class_=class_name)
+    if element_with_class:
+        # Отримуємо текстове значення елементу
+        list_all_value = []
+        for element in element_with_class:
+            value = element.text.strip()
+            list_all_value.append(value)
+        else:
+            print(f"Елементи з класом '{class_name}' не знайдено на сторінці.")
+
+    table = soup.find('table', class_= 'sc-1x32wa2-1')
+    print(table)
+else:
+    print("Не вдалося отримати сторінку")
+
+
