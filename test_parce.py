887d6c10a8de7cc3eef8097f9b13142e799da97e
Staravoy <85824511+Staravoy@users.noreply.github.com>
2023-09-04 23:08:25 +0300

    backend finish

--- test_parce.py
+++ test_parce.py
@@ -1,8 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
-import pandas as pd
-import numpy as np
 import re
+
+def average(pattern, list_all):
+    usd_values = [re.search(pattern, item).group(1) for item in list_all if re.search(pattern, item)]
+
+    list_float = []
+    for i in usd_values:
+        list_float.append(float(i.replace(',', '.')))
+    total = sum(list_float)
+    midl_num = total / len(list_float)
+    return midl_num
 
 # URL сторінки
 url = "https://minfin.com.ua/ua/currency/"
@@ -27,13 +35,24 @@ if response.status_code == 200:
             value = element.text.strip()
             list_all_value.append(value)
         else:
-            print(f"Елементи з класом '{class_name}' не знайдено на сторінці.")
-
+            pass
+            # print(f"Елементи з класом '{class_name}' не знайдено на сторінці.")
+    currency = input('Enter currency (USD or EUR):')
+    if currency == 'USD':
         pattern = re.compile(r"USD(\d+,\d+)")
-        usd_values = [re.search(pattern, item).group(1) for item in list_all_value if re.search(pattern, item)]
-        print(usd_values)
-# table = soup.find('table', class_='sc-1x32wa2-1')
-# print(table)
+    elif currency == 'EUR':
+        pattern = re.compile(r"EUR(\d+,\d+)")
+    else:
+        print('This currency not find.')
+
+    numbers_money = input('Enter numbers:')
+    average_num = average(pattern, list_all_value)
+    result = int(numbers_money) * average_num
+    print(f'Result of converter: {round(result, 2)}')
+
+
+
+
 else:
     print("Не вдалося отримати сторінку")
 
