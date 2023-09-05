cf4da40f1d116836f614ea54454b1c7539b5823c
Staravoy <85824511+Staravoy@users.noreply.github.com>
2023-09-05 17:31:29 +0300

    90%beckend enaugh

--- test_parce.py
+++ test_parce.py
@@ -4,6 +4,15 @@ import pandas as pd
 import numpy as np
 import re
 
+def middle_num(usd_values):
+    total = []
+    for i in usd_values:
+        i = round(float(i.replace(',', '.')),2)
+        total.append(i)
+    sum_all_num = sum(total)
+    middle = sum_all_num/len(total)
+    return round(middle, 2)
+
 # URL сторінки
 url = "https://minfin.com.ua/ua/currency/"
 
@@ -27,13 +36,21 @@ if response.status_code == 200:
             value = element.text.strip()
             list_all_value.append(value)
         else:
-            print(f"Елементи з класом '{class_name}' не знайдено на сторінці.")
+            pass
+            # print(f"Елементи з класом '{class_name}' не знайдено на сторінці.")
 
-        pattern = re.compile(r"USD(\d+,\d+)")
-        usd_values = [re.search(pattern, item).group(1) for item in list_all_value if re.search(pattern, item)]
-        print(usd_values)
-# table = soup.find('table', class_='sc-1x32wa2-1')
-# print(table)
+        type_current = input('Enter USD or EUR:')
+        if type_current == 'USD':
+            pattern = re.compile(r"USD(\d+,\d+)")
+            usd_values = [re.search(pattern, item).group(1) for item in list_all_value if re.search(pattern, item)]
+            middle = middle_num(usd_values)
+        elif type_current == 'EUR':
+            pattern = re.compile(r"EUR(\d+,\d+)")
+            eur_values = [re.search(pattern, item).group(1) for item in list_all_value if re.search(pattern, item)]
+            middle = middle_num(eur_values)
+        amount_currency = input('Enter amount currency:')
+        result = int(amount_currency) * middle
+        print(f'Result = {result}')
 else:
     print("Не вдалося отримати сторінку")
 
